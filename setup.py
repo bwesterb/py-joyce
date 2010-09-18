c9458c639ba403b0a088ac3fbe65f553d2e3c9c7
Bas Westerbaan <bas@westerbaan.name>
2010-09-18 19:35:46 +0200

    Initial (empty) version
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from get_git_version import get_git_version
+
+setup(name='joyce',
+      version=get_git_version(),
+      description='JSON channels',
+      author='Bas Westerbaan',
+      author_email='bas@westerbaan.name',
+      url='http://github.com/bwesterb/py-joyce/',
+      packages=['joyce'],
+      package_dir={'joyce': 'src'},
+      install_requires = ['docutils>=0.3',
+	      		  'sarah>=0.1.0a1',
+			  'pymongo>=1.8.1',]
+      )
