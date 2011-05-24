04899503db12a02e06b7b9b2826e6cc938b1bb5c
Bas Westerbaan <bas@westerbaan.name>
2011-05-24 16:42:11 +0200

    Expand tabs to spaces
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- src/base.py
+++ src/base.py
@@ -8,123 +8,123 @@ import logging
 import threading
 
 class JoyceError(Exception):
-	pass
+        pass
 class HijackedChannel(JoyceError):
-	pass
+        pass
 class UnsupportedProtocol(JoyceError):
-	pass
+        pass
 
 class JoyceChannel(object):
-	def __init__(self, relay, token, logger):
-		self.relay = relay
-		self.l = logger
-		self.token = token
-		self.on_message = Event()
-		self.on_stream = Event()
-	def send_stream(self, stream, blocking=True):
-		self.relay.send_stream(self.token, stream, blocking)
-	def send_message(self, d):
-		self.relay.send_message(self.token, d)
-	def handle_message(self, d):
-		self.on_message(self, d)
-	def handle_stream(self, stream):
-		self.on_stream(self, stream)
-	def close(self):
-		self.relay.close_channel(self.token)
+        def __init__(self, relay, token, logger):
+                self.relay = relay
+                self.l = logger
+                self.token = token
+                self.on_message = Event()
+                self.on_stream = Event()
+        def send_stream(self, stream, blocking=True):
+                self.relay.send_stream(self.token, stream, blocking)
+        def send_message(self, d):
+                self.relay.send_message(self.token, d)
+        def handle_message(self, d):
+                self.on_message(self, d)
+        def handle_stream(self, stream):
+                self.on_stream(self, stream)
+        def close(self):
+                self.relay.close_channel(self.token)
         def after_close(self):
                 pass
 
 class JoyceRelay(object):
-	def __init__(self, hub, logger):
-		self.l = logger
-		self.hub = hub
-	def send_message(self, token, d):
-		raise NotImplementedError
-	def send_stream(self, token, stream, blocking=True):
-		raise NotImplementedError
-	def handle_message(self, token, d):
-		self.hub.handle_message(token, d, self)
-	def handle_stream(self, token, stream):
-		self.hub.handle_stream(token, stream, self)
-	def close_channel(self, token):
-		self.hub.remove_channel(token)
+        def __init__(self, hub, logger):
+                self.l = logger
+                self.hub = hub
+        def send_message(self, token, d):
+                raise NotImplementedError
+        def send_stream(self, token, stream, blocking=True):
+                raise NotImplementedError
+        def handle_message(self, token, d):
+                self.hub.handle_message(token, d, self)
+        def handle_stream(self, token, stream):
+                self.hub.handle_stream(token, stream, self)
+        def close_channel(self, token):
+                self.hub.remove_channel(token)
 
 class JoyceHub(Module):
-	def __init__(self, channel_class=None, *args, **kwargs):
-		super(JoyceHub, self).__init__(*args, **kwargs)
-		self.channel_class = (JoyceChannel
-				if channel_class is None else channel_class)
-		self.lock = threading.Lock()
-		self.channel_to_relay = dict()
-		self.relay_to_channels = dict()
-		self.channels = dict()
-		self.on_new_channel = Event()
-	def _get_channel_for_relay(self, token, relay):
-		new_channel = False
-		with self.lock:
+        def __init__(self, channel_class=None, *args, **kwargs):
+                super(JoyceHub, self).__init__(*args, **kwargs)
+                self.channel_class = (JoyceChannel
+                                if channel_class is None else channel_class)
+                self.lock = threading.Lock()
+                self.channel_to_relay = dict()
+                self.relay_to_channels = dict()
+                self.channels = dict()
+                self.on_new_channel = Event()
+        def _get_channel_for_relay(self, token, relay):
+                new_channel = False
+                with self.lock:
                         if self.channels.get(token) is None:
-				c = self._create_channel(token, relay)
-				new_channel = True
-			else:
-				if relay != self.channel_to_relay[token]:
-					raise HijackedChannel
-				c = self.channels[token]
-		if new_channel:
-			self.on_new_channel(c)
-		return c
-	def handle_stream(self, token, stream, relay):
-		c = self._get_channel_for_relay(token, relay)
-		c.handle_stream(stream)
-	def handle_message(self, token, d, relay):
-		c = self._get_channel_for_relay(token, relay)
-		c.handle_message(d)
-	def broadcast_message(self, d):
-		with self.lock:
-			channels = self.channels.values()
-		for channel in channels:
-			channel.send_message(d)
-	
-	def _create_channel(self, token, relay, channel_class=None):
-		if token is None:
-			token = self._generate_token()
-		if channel_class is None:
-			channel_class = self.channel_class
-		channel = channel_class(relay, token,
-			logging.getLogger("%s.%s" % (self.l.name, token)))
-		self.channel_to_relay[token] = relay
-		if not relay in self.relay_to_channels:
-			self.relay_to_channels[relay] = set()
-		self.relay_to_channels[relay].add(token)
-		self.channels[token] = channel
-		return channel
-	def _generate_token(self):
-		while True:
-			_try = base64.b64encode(os.urandom(6))
-			if not _try in self.channels:
-				self.channels[_try] = None
-				return _try
-	def remove_channel(self, token):
-		with self.lock:
+                                c = self._create_channel(token, relay)
+                                new_channel = True
+                        else:
+                                if relay != self.channel_to_relay[token]:
+                                        raise HijackedChannel
+                                c = self.channels[token]
+                if new_channel:
+                        self.on_new_channel(c)
+                return c
+        def handle_stream(self, token, stream, relay):
+                c = self._get_channel_for_relay(token, relay)
+                c.handle_stream(stream)
+        def handle_message(self, token, d, relay):
+                c = self._get_channel_for_relay(token, relay)
+                c.handle_message(d)
+        def broadcast_message(self, d):
+                with self.lock:
+                        channels = self.channels.values()
+                for channel in channels:
+                        channel.send_message(d)
+        
+        def _create_channel(self, token, relay, channel_class=None):
+                if token is None:
+                        token = self._generate_token()
+                if channel_class is None:
+                        channel_class = self.channel_class
+                channel = channel_class(relay, token,
+                        logging.getLogger("%s.%s" % (self.l.name, token)))
+                self.channel_to_relay[token] = relay
+                if not relay in self.relay_to_channels:
+                        self.relay_to_channels[relay] = set()
+                self.relay_to_channels[relay].add(token)
+                self.channels[token] = channel
+                return channel
+        def _generate_token(self):
+                while True:
+                        _try = base64.b64encode(os.urandom(6))
+                        if not _try in self.channels:
+                                self.channels[_try] = None
+                                return _try
+        def remove_channel(self, token):
+                with self.lock:
                         c = self.channels[token]
-			del self.channels[token]
-			relay = self.channel_to_relay[token]
-			self.relay_to_channels[relay].remove(token)
-			del self.channel_to_relay[token]
+                        del self.channels[token]
+                        relay = self.channel_to_relay[token]
+                        self.relay_to_channels[relay].remove(token)
+                        del self.channel_to_relay[token]
                 c.after_close()
-	def remove_relay(self, relay):
-		with self.lock:
-			cs = self.relay_to_channels[relay]
-			for t in cs:
-				del self.channels[t]
-				assert self.channel_to_relay[t] is relay
-				del self.channel_to_relay[t]
-			del self.relay_to_channels[relay]
+        def remove_relay(self, relay):
+                with self.lock:
+                        cs = self.relay_to_channels[relay]
+                        for t in cs:
+                                del self.channels[t]
+                                assert self.channel_to_relay[t] is relay
+                                del self.channel_to_relay[t]
+                        del self.relay_to_channels[relay]
 
 class JoyceClient(JoyceHub):
-	def __init__(self, *args, **kwargs):
-		super(JoyceClient, self).__init__(None, *args, **kwargs)
-	def create_channel(self, token=None, channel_class=None):
-		raise NotImplementedError
+        def __init__(self, *args, **kwargs):
+                super(JoyceClient, self).__init__(None, *args, **kwargs)
+        def create_channel(self, token=None, channel_class=None):
+                raise NotImplementedError
 class JoyceServer(JoyceHub):
-	def __init__(self, *args, **kwargs):
-		super(JoyceServer, self).__init__(None, *args, **kwargs)
+        def __init__(self, *args, **kwargs):
+                super(JoyceServer, self).__init__(None, *args, **kwargs)
--- src/light.py
+++ src/light.py
@@ -1,5 +1,5 @@
 from joyce.base import JoyceServer, JoyceClient, JoyceRelay, JoyceHub, \
-			UnsupportedProtocol
+                        UnsupportedProtocol
 
 from sarah.socketServer import TCPSocketServer
 from sarah.io import IntSocketFile
@@ -17,193 +17,193 @@ import threading
 import cStringIO as StringIO
 
 class LightJoyceRelay(JoyceRelay):
-	def __init__(self, hub, logger, f, protocol):
-		super(LightJoyceRelay, self).__init__(hub, logger)
-		self.running = True
-		self.queue = []
-		self.cond = threading.Condition()
-		self.f = f
-		self.protocol = protocol
-		self.cleaned = False
-		self.prot_map = {
-			'json1': (self.read_json1, self.write_json1),
-			'json1gz': (self.read_json1gz, self.write_json1gz),
-			'bson1': (self.read_bson1, self.write_bson1),
-			'bson1gz': (self.read_bson1gz, self.write_bson1gz)}
-	def send_message(self, token, d):
-		with self.cond:
-			self.queue.append((token, d))
-			self.cond.notify()
-	
-	def cleanup(self):
-		with self.cond:
-			if self.cleaned:
-				return
-			self.cleaned = True
-		self.f.close()
-		self.hub.remove_relay(self)
-	def interrupt(self):
-		with self.cond:
-			self.running = False
-			self.cond.notifyAll()
-		self.f.interrupt()
-		self.cleanup()
-	def _run_sender(self):
-		self.cond.acquire()
-		while self.running:
-			while not self.queue and self.running:
-				self.cond.wait()
-			if not self.running:
-				break
-			queue = list(self.queue)
-			self.queue = []
-			self.cond.release()
-			try:
-				self.prot_map[self.protocol][1](queue)
-			except Exception as e:
-				self.l.exception("Uncaught exception")
-				break
-			finally:
-				self.cond.acquire()
-		running = self.running
-		self.cond.release()
-		if running:
-			self.interrupt()
-	def _run_receiver(self):
-		try:
-			self.prot_map[self.protocol][0]()
-		except Exception as e:
-			self.l.exception("Uncaught exception")
-		if self.running:
-			self.interrupt()
-	def _read_json1(self, raw_d):
-		self._read_son1(json.loads(raw_d))
-	def _read_son1(self, d):
-		if not isinstance(d, list) or len(d) != 2:
-			self.f.write("Expected 2el list\n")
-			return
-		if not isinstance(d[0], str) or d[0] is None:
-			self.f.write("Expected token\n")
-			return
-		self.handle_message(*d)
-	def write_json1(self, ds):
-		for token, d in ds:
-			self.f.write(json.dumps([token, d]))
-			self.f.write("\n")
-	def read_json1(self):
-		while self.running:
-			self._read_json1(self.f.readline()[:-1])
-	def write_json1gz(self, ds):
-		io = StringIO.StringIO()
-		first = True
-		for token, d in ds:
-			if first: first = False
-			else: io.write("\n")
-			io.write(json.dumps([token, d]))
-		ret = zlib.compress(io.getvalue(), 9)
-		write_packed_int(self.f, len(ret))
-		self.f.write(ret)
-	def read_json1gz(self):
-		while self.running:
-			c = read_packed_int(self.f)
-			for m in zlib.decompress(
-					self.f.read(c)).split("\n"):
-				self._read_json1(m)
-	def _read_bson1(self, raw_d):
-		v = pymongo.bson.BSON(raw_d).to_dict()
-		self.handle_message(v.get('t'), v.get('d'))
-	def write_bson1(self, ds):
-		for token, d in ds:
-			_d = {'t': token, 'd': d} 
-			tmp = str(pymongo.bson.BSON.from_dict(_d))
-			write_packed_int(self.f, len(tmp))
-			self.f.write(tmp)
-	def read_bson1(self):
-		while self.running:
-			c = read_packed_int(self.f)
-			self._read_bson1(self.f.read(c))
-	def write_bson1gz(self, ds):
-		io = StringIO.StringIO()
-		for token, d in ds:
-			_d = {'t': token, 'd':d}
-			tmp = str(pymongo.bson.BSON.from_dict(_d))
-			write_packed_int(io, len(tmp))
-			io.write(tmp)
-		tmp = zlib.compress(io.getvalue(), 9)
-		write_packed_int(self.f, len(tmp))
-		self.f.write(tmp)
-	def read_bson1gz(self):
-		while self.running:
-			c = read_packed_int(self.f)
-			s = zlib.decompress(self.f.read(c))
-			io = StringIO.StringIO(s)
-			while io.tell() != len(s):
-				c = read_packed_int(io)
-				self._read_bson1(io.read(c))
+        def __init__(self, hub, logger, f, protocol):
+                super(LightJoyceRelay, self).__init__(hub, logger)
+                self.running = True
+                self.queue = []
+                self.cond = threading.Condition()
+                self.f = f
+                self.protocol = protocol
+                self.cleaned = False
+                self.prot_map = {
+                        'json1': (self.read_json1, self.write_json1),
+                        'json1gz': (self.read_json1gz, self.write_json1gz),
+                        'bson1': (self.read_bson1, self.write_bson1),
+                        'bson1gz': (self.read_bson1gz, self.write_bson1gz)}
+        def send_message(self, token, d):
+                with self.cond:
+                        self.queue.append((token, d))
+                        self.cond.notify()
+        
+        def cleanup(self):
+                with self.cond:
+                        if self.cleaned:
+                                return
+                        self.cleaned = True
+                self.f.close()
+                self.hub.remove_relay(self)
+        def interrupt(self):
+                with self.cond:
+                        self.running = False
+                        self.cond.notifyAll()
+                self.f.interrupt()
+                self.cleanup()
+        def _run_sender(self):
+                self.cond.acquire()
+                while self.running:
+                        while not self.queue and self.running:
+                                self.cond.wait()
+                        if not self.running:
+                                break
+                        queue = list(self.queue)
+                        self.queue = []
+                        self.cond.release()
+                        try:
+                                self.prot_map[self.protocol][1](queue)
+                        except Exception as e:
+                                self.l.exception("Uncaught exception")
+                                break
+                        finally:
+                                self.cond.acquire()
+                running = self.running
+                self.cond.release()
+                if running:
+                        self.interrupt()
+        def _run_receiver(self):
+                try:
+                        self.prot_map[self.protocol][0]()
+                except Exception as e:
+                        self.l.exception("Uncaught exception")
+                if self.running:
+                        self.interrupt()
+        def _read_json1(self, raw_d):
+                self._read_son1(json.loads(raw_d))
+        def _read_son1(self, d):
+                if not isinstance(d, list) or len(d) != 2:
+                        self.f.write("Expected 2el list\n")
+                        return
+                if not isinstance(d[0], str) or d[0] is None:
+                        self.f.write("Expected token\n")
+                        return
+                self.handle_message(*d)
+        def write_json1(self, ds):
+                for token, d in ds:
+                        self.f.write(json.dumps([token, d]))
+                        self.f.write("\n")
+        def read_json1(self):
+                while self.running:
+                        self._read_json1(self.f.readline()[:-1])
+        def write_json1gz(self, ds):
+                io = StringIO.StringIO()
+                first = True
+                for token, d in ds:
+                        if first: first = False
+                        else: io.write("\n")
+                        io.write(json.dumps([token, d]))
+                ret = zlib.compress(io.getvalue(), 9)
+                write_packed_int(self.f, len(ret))
+                self.f.write(ret)
+        def read_json1gz(self):
+                while self.running:
+                        c = read_packed_int(self.f)
+                        for m in zlib.decompress(
+                                        self.f.read(c)).split("\n"):
+                                self._read_json1(m)
+        def _read_bson1(self, raw_d):
+                v = pymongo.bson.BSON(raw_d).to_dict()
+                self.handle_message(v.get('t'), v.get('d'))
+        def write_bson1(self, ds):
+                for token, d in ds:
+                        _d = {'t': token, 'd': d} 
+                        tmp = str(pymongo.bson.BSON.from_dict(_d))
+                        write_packed_int(self.f, len(tmp))
+                        self.f.write(tmp)
+        def read_bson1(self):
+                while self.running:
+                        c = read_packed_int(self.f)
+                        self._read_bson1(self.f.read(c))
+        def write_bson1gz(self, ds):
+                io = StringIO.StringIO()
+                for token, d in ds:
+                        _d = {'t': token, 'd':d}
+                        tmp = str(pymongo.bson.BSON.from_dict(_d))
+                        write_packed_int(io, len(tmp))
+                        io.write(tmp)
+                tmp = zlib.compress(io.getvalue(), 9)
+                write_packed_int(self.f, len(tmp))
+                self.f.write(tmp)
+        def read_bson1gz(self):
+                while self.running:
+                        c = read_packed_int(self.f)
+                        s = zlib.decompress(self.f.read(c))
+                        io = StringIO.StringIO(s)
+                        while io.tell() != len(s):
+                                c = read_packed_int(io)
+                                self._read_bson1(io.read(c))
 
 class LightJoyceClient(JoyceClient):
-	def __init__(self, *args, **kwargs):
-		super(LightJoyceClient, self).__init__(*args, **kwargs)
-		self.relay = None
-		self.connected = threading.Event()
-		self.running = True
-	def create_channel(self, token=None, channel_class=None):
-		while self.relay is None:
-			self.l.warn("create_channel: not connected yet")
-			self.connected.wait()
-			if not self.running:
-				return
-		with self.lock:
-			return self._create_channel(token, self.relay,
-					channel_class)
-	def run(self):
-		self.do_handshake()
-		self.threadPool.execute_named(self.relay._run_sender,
-				"%s _run_server" % self.l.name)
-		self.relay._run_receiver()
-	def do_handshake(self):
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((self.host, self.port))
-		f = IntSocketFile(s)
-		prots = json.loads(f.readline()[:-1])
-		if not self.protocol in prots:
-			raise UnsupportedProtocol
-		f.write(json.dumps(self.protocol))
-		f.write("\n")
-		l = logging.getLogger("%s.relay" % self.l.name)
-		self.relay = LightJoyceRelay(self, l, f, self.protocol)
-		self.connected.set()
-	def stop(self):
-		self.running = False
-		self.connected.set()
-		self.relay.interrupt()
-		self.relay.cleanup()
+        def __init__(self, *args, **kwargs):
+                super(LightJoyceClient, self).__init__(*args, **kwargs)
+                self.relay = None
+                self.connected = threading.Event()
+                self.running = True
+        def create_channel(self, token=None, channel_class=None):
+                while self.relay is None:
+                        self.l.warn("create_channel: not connected yet")
+                        self.connected.wait()
+                        if not self.running:
+                                return
+                with self.lock:
+                        return self._create_channel(token, self.relay,
+                                        channel_class)
+        def run(self):
+                self.do_handshake()
+                self.threadPool.execute_named(self.relay._run_sender,
+                                "%s _run_server" % self.l.name)
+                self.relay._run_receiver()
+        def do_handshake(self):
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((self.host, self.port))
+                f = IntSocketFile(s)
+                prots = json.loads(f.readline()[:-1])
+                if not self.protocol in prots:
+                        raise UnsupportedProtocol
+                f.write(json.dumps(self.protocol))
+                f.write("\n")
+                l = logging.getLogger("%s.relay" % self.l.name)
+                self.relay = LightJoyceRelay(self, l, f, self.protocol)
+                self.connected.set()
+        def stop(self):
+                self.running = False
+                self.connected.set()
+                self.relay.interrupt()
+                self.relay.cleanup()
 
 class LightJoyceServer(TCPSocketServer, JoyceServer):
-	class Handler(object):
-		def __init__(self, hub, conn, addr, logger):
-			self.hub = hub
-			self.conn = conn
-			self.l = logger
-		def handle(self):
-			f = IntSocketFile(self.conn)
-			self.relay = LightJoyceRelay(self.hub, self.l, f, None)
-			f.write(json.dumps(self.relay.prot_map.keys()))
-			f.write("\n")
-			protocol = self.protocol = json.loads(
-					f.readline()[:-1])
-			if not isinstance(protocol, basestring) or \
-					not protocol in self.relay.prot_map:
-				self.f.write("Invalid response\n")
-				return
-			self.relay.protocol = protocol
-			self.hub.threadPool.execute(self.relay._run_sender)
-			self.relay._run_receiver()
-		def cleanup(self):
-			self.relay.cleanup()
-		def interrupt(self):
-			self.relay.interrupt()
-	def __init__(self, *args, **kwargs):
-		super(LightJoyceServer, self).__init__(*args, **kwargs)
-	def create_handler(self, con, addr, logger):
-		return LightJoyceServer.Handler(self, con, addr, logger)
+        class Handler(object):
+                def __init__(self, hub, conn, addr, logger):
+                        self.hub = hub
+                        self.conn = conn
+                        self.l = logger
+                def handle(self):
+                        f = IntSocketFile(self.conn)
+                        self.relay = LightJoyceRelay(self.hub, self.l, f, None)
+                        f.write(json.dumps(self.relay.prot_map.keys()))
+                        f.write("\n")
+                        protocol = self.protocol = json.loads(
+                                        f.readline()[:-1])
+                        if not isinstance(protocol, basestring) or \
+                                        not protocol in self.relay.prot_map:
+                                self.f.write("Invalid response\n")
+                                return
+                        self.relay.protocol = protocol
+                        self.hub.threadPool.execute(self.relay._run_sender)
+                        self.relay._run_receiver()
+                def cleanup(self):
+                        self.relay.cleanup()
+                def interrupt(self):
+                        self.relay.interrupt()
+        def __init__(self, *args, **kwargs):
+                super(LightJoyceServer, self).__init__(*args, **kwargs)
+        def create_handler(self, con, addr, logger):
+                return LightJoyceServer.Handler(self, con, addr, logger)
