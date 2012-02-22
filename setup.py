675b526619b85f7f7b5cbe88ff5b4512fa379f0f
Bas Westerbaan <bas@westerbaan.name>
2012-02-22 12:38:18 +0100

    (bugfix of)^3 include .mirte files
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages, findall
+from setuptools import setup, find_packages
 from get_git_version import get_git_version
+import os, os.path
+
+def find_package_data():
+    base = os.path.join(os.path.dirname(__file__), 'src')
+    s, r = ['.'], []
+    while s:
+        p = s.pop()
+        for c in os.listdir(os.path.join(base, p)):
+            if os.path.isdir(os.path.join(base, p, c)):
+                s.append(os.path.join(p, c))
+            elif c.endswith('.mirte'):
+                r.append(os.path.join(p, c))
+    return r
 
 setup(name='joyce',
       version=get_git_version(),
@@ -11,8 +24,7 @@ setup(name='joyce',
       url='http://github.com/bwesterb/py-joyce/',
       packages=['joyce'],
       package_dir={'joyce': 'src'},
-      package_data={
-          'joyce': [f for f in findall('joyce') if f.endswith('.mirte')] },
+      package_data={'joyce': find_package_data()},
       install_requires = ['docutils>=0.3',
                           'sarah>=0.1.1',
                           'mirte>=0.1.1',
