9ecacfe3c8c7b6422d7e2188d0f6245a313b4736
Bas Westerbaan <bas@westerbaan.name>
2012-02-22 11:59:45 +0100

    bugfix of the bugfix: another way to include .mirte files
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, findall
 from get_git_version import get_git_version
 
 setup(name='joyce',
@@ -11,6 +11,8 @@ setup(name='joyce',
       url='http://github.com/bwesterb/py-joyce/',
       packages=['joyce'],
       package_dir={'joyce': 'src'},
+      package_data={
+          'joyce': [f for f in findall('joyce') if f.endswith('.mirte')] },
       install_requires = ['docutils>=0.3',
                           'sarah>=0.1.1',
                           'mirte>=0.1.1',
